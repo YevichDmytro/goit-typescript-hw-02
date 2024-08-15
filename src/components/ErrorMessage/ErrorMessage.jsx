230e2a9303ab3c67e54171f7f2f12ee8a061c6eb
Dmytro <57775385+YevichDmytro@users.noreply.github.com>
2024-08-15 19:54:34 +0300

    start

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -5,11 +5,6 @@ const ErrorMessage = () => {
     <div className={style.errorWrap}>
       <h1>Oops, something went wrong!</h1>
       <p>Try reloading your page.</p>
-      <img
-        src='../../../public/error-img.jpg'
-        alt='error image'
-        width='370px'
-      />
     </div>
   );
 };
