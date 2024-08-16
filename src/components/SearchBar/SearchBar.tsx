9ff0e41617d98939f84ed9ccbd90739364d367f6
Dmytro <57775385+YevichDmytro@users.noreply.github.com>
2024-08-16 12:21:13 +0300

    SearchBar fix

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent } from 'react';
 import { Button, TextField } from '@mui/material';
 import toast, { Toaster } from 'react-hot-toast';
 import style from './SearchBar.module.css';
@@ -8,36 +8,40 @@ type Props = {
 };
 
 const SearchBar: React.FC<Props> = ({ onSearch }) => {
-  const [topic, setTopic] = useState<string>('');
-
   const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
 
-    if (topic === '') {
-      toast.error('Field are empty!');
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+    const query = formData.get('search');
+
+    if (typeof query === 'string' && query.trim()) {
+      onSearch(query);
+      form.reset();
     } else {
-      onSearch(topic);
+      toast.error('Field are empty!');
     }
   };
 
   return (
-    <header className={style.header}>
-      <Toaster />
-      <form onSubmit={handleSubmit} className={style.form}>
-        <TextField
-          id='outlined-basic'
-          variant='outlined'
-          label='Search images and photos'
-          type='text'
-          name='search'
-          autoComplete='on'
-          onChange={e => setTopic(e.target.value.trim().toLowerCase())}
-        />
-        <Button variant='contained' type='submit'>
-          Search
-        </Button>
-      </form>
-    </header>
+    <>
+      <header className={style.header}>
+        <form onSubmit={handleSubmit} className={style.form}>
+          <TextField
+            id='outlined-basic'
+            variant='outlined'
+            label='Search images and photos'
+            type='text'
+            name='search'
+            autoComplete='on'
+          />
+          <Button variant='contained' type='submit'>
+            Search
+          </Button>
+        </form>
+      </header>
+      <Toaster position='top-right' />
+    </>
   );
 };
 
